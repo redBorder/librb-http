cca577cb6298caf7bf45f79157a0d67fc76feff2
Diego Fernández Barrera <bigomby@gmail.com>
2015-09-18 12:45:14 +0200

    Implemented CURL on a worker thread.

---
+++ src/librb-http.h
@@ -0,0 +1,15 @@
+////////////////////
+// Structures
+////////////////////
+
+struct rb_http_handler_t;
+
+////////////////////
+/// Functions
+////////////////////
+struct rb_http_handler_t * rb_http_handler (char * urls_str);
+void rb_http_produce (struct rb_http_handler_t * handler,
+                      char * message/*,
+                      int options*/);
+void rb_http_init();
+void rb_http_clean();
--- src/server_handler.c
+++
@@ -1,2 +0,0 @@
-#include "librb-http.h"
-#include <stdio.h>
