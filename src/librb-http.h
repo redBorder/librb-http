e30b34c0a0c65e7e078cbedcc9eb04be711b42d7
Diego Fernández Barrera <bigomby@gmail.com>
2015-09-28 12:07:10 +0200

    Added reporting feature

--- src/librb-http.h
+++ src/librb-http.h
@@ -8,6 +8,11 @@
 ////////////////////
 struct rb_http_handler_s;
 struct rb_http_message_s;
+typedef void (*cb_report) (struct rb_http_handler_s * rb_http_handler,
+                           int status_code,
+                           const char * status_code_str,
+                           char * buff,
+                           void * opaque);
 
 ////////////////////
 /// Functions
@@ -19,3 +24,5 @@ void rb_http_produce (struct rb_http_handler_s * handler,
                       char * buff,
                       size_t len,
                       int flags);
+void rb_http_get_reports (struct rb_http_handler_s * rb_http_handler,
+                          cb_report report_fn);
