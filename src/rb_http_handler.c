15673344e6f68054562de9c57aec601596c2de0a
Diego Fernández Barrera <bigomby@gmail.com>
2015-09-28 10:06:44 +0200

    New thread to handle received messages.

--- src/rb_http_handler.c
+++ src/rb_http_handler.c
@@ -30,10 +30,11 @@ struct rb_http_handler_s {
 	int msgs_left;
 	long curlmopt_maxconnects;
 	char ** urls;
+	pthread_mutex_t multi_handle_mutex;
 	CURLM * multi_handle;
-	CURLMsg * msg;
 	rd_fifoq_t rfq;
-	rd_thread_t * rd_thread;
+	rd_thread_t * rd_thread_send;
+	rd_thread_t * rd_thread_recv;
 };
 
 /**
@@ -52,6 +53,7 @@ struct rb_http_message_s {
 ////////////////////
 static char** str_split (char* a_str, const char a_delim);
 static void * curl_send_message (void * arg);
+static void * curl_recv_message (void * arg);
 
 /**
  * @brief Creates a handler to produce messages.
@@ -73,14 +75,18 @@ struct rb_http_handler_s * rb_http_handler (char * urls_str,
 
 		rb_http_handler->urls = str_split (urls_str, ',');
 		rb_http_handler->urls[0] = urls_str;
-		rb_http_handler->still_running = 1;
-		rb_http_handler->msg = NULL;
+		rb_http_handler->still_running = 0;
 		rb_http_handler->msgs_left = 0;
+		pthread_mutex_init (&rb_http_handler->multi_handle_mutex, NULL);
 		rb_http_handler->multi_handle = curl_multi_init();
 		rb_http_handler->thread_running = 1;
 
-		rd_thread_create (&rb_http_handler->rd_thread, "curl_send_message", NULL,
+		rd_thread_create (&rb_http_handler->rd_thread_send, "curl_send_message", NULL,
 		                  curl_send_message,
+		                  rb_http_handler);
+
+		rd_thread_create (&rb_http_handler->rd_thread_recv, "curl_recv_message", NULL,
+		                  curl_recv_message,
 		                  rb_http_handler);
 
 		return rb_http_handler;
@@ -95,9 +101,11 @@ struct rb_http_handler_s * rb_http_handler (char * urls_str,
  */
 void rb_http_handler_destroy (struct rb_http_handler_s * rb_http_handler) {
 	rb_http_handler->thread_running = 0;
-	rd_thread_kill_join (rb_http_handler->rd_thread, NULL);
+	rd_thread_kill_join (rb_http_handler->rd_thread_send, NULL);
+	rd_thread_kill_join (rb_http_handler->rd_thread_recv, NULL);
 
 	curl_multi_cleanup (rb_http_handler->multi_handle);
+	pthread_mutex_destroy (&rb_http_handler->multi_handle_mutex);
 	rd_fifoq_destroy (&rb_http_handler->rfq);
 
 	if (rb_http_handler->urls) {
@@ -108,7 +116,6 @@ void rb_http_handler_destroy (struct rb_http_handler_s * rb_http_handler) {
 		free (rb_http_handler->urls);
 	}
 
-	free (rb_http_handler->msg);
 	free (rb_http_handler);
 }
 
@@ -153,24 +160,25 @@ void rb_http_produce (struct rb_http_handler_s * handler,
  */
 void * curl_send_message (void * arg) {
 
-	struct rb_http_handler_s * rb_http_handler = (struct rb_http_handler_s *)
-	        arg;
+	rd_thread_sigmask (SIG_BLOCK, SIGINT, RD_SIG_END);
+	struct rb_http_handler_s * rb_http_handler = (struct rb_http_handler_s *) arg;
 
-	CURL * handle = NULL;
 	struct rb_http_message_s * message = NULL;
 	rd_fifoq_elm_t * rfqe = NULL;
+	CURL * handler;
 
 	if (arg != NULL) {
 		while (rb_http_handler->thread_running) {
 			rfqe = rd_fifoq_pop (&rb_http_handler->rfq);
 			if (rfqe != NULL && rfqe->rfqe_ptr != NULL) {
 				message = rfqe->rfqe_ptr;
+				rd_fifoq_elm_release (&rb_http_handler->rfq, rfqe);
 
-				handle =  curl_easy_init();
-				curl_easy_setopt (handle, CURLOPT_URL, rb_http_handler->urls[0]);
-				curl_easy_setopt (handle, CURLOPT_TIMEOUT_MS, 3000L);
-				curl_easy_setopt (handle, CURLOPT_FAILONERROR, 1L);
-				// curl_easy_setopt (handle, CURLOPT_VERBOSE, 1L);
+				handler  =  curl_easy_init();
+				curl_easy_setopt (handler, CURLOPT_URL,
+				                  rb_http_handler->urls[0]);
+				// curl_easy_setopt (handler, CURLOPT_FAILONERROR, 1L);
+				// curl_easy_setopt (handler, CURLOPT_VERBOSE, 1L);
 
 				struct curl_slist * headers = NULL;
 				headers = curl_slist_append (headers, "Accept: application/json");
@@ -178,94 +186,16 @@ void * curl_send_message (void * arg) {
 				                             "Content-Type: application/json");
 				headers = curl_slist_append (headers, "charsets: utf-8");
 
-				curl_easy_setopt (handle, CURLOPT_HTTPHEADER, headers);
-				curl_easy_setopt (handle, CURLOPT_POSTFIELDS, message->payload);
+				curl_easy_setopt (handler, CURLOPT_HTTPHEADER, headers);
+				curl_easy_setopt (handler, CURLOPT_POSTFIELDS,
+				                  message->payload);
 
-				curl_multi_add_handle (rb_http_handler->multi_handle, handle);
+				pthread_mutex_lock (&rb_http_handler->multi_handle_mutex);
+				curl_multi_add_handle (rb_http_handler->multi_handle, handler);
 				curl_multi_perform (rb_http_handler->multi_handle,
 				                    &rb_http_handler->still_running);
-
-				do {
-					struct timeval timeout;
-					int rc; /* select() return code */
-					CURLMcode mc; /* curl_multi_fdset() return code */
-
-					fd_set fdread;
-					fd_set fdwrite;
-					fd_set fdexcep;
-					int maxfd = -1;
-
-					long curl_timeo = -1;
-
-					FD_ZERO (&fdread);
-					FD_ZERO (&fdwrite);
-					FD_ZERO (&fdexcep);
-
-					/* set a suitable timeout to play around with */
-					timeout.tv_sec = 1;
-					timeout.tv_usec = 0;
-
-					curl_multi_timeout (rb_http_handler->multi_handle, &curl_timeo);
-					if (curl_timeo >= 0) {
-						timeout.tv_sec = curl_timeo / 1000;
-						if (timeout.tv_sec > 1)
-							timeout.tv_sec = 1;
-						else
-							timeout.tv_usec = (curl_timeo % 1000) * 1000;
-					}
-
-					/* get file descriptors from the transfers */
-					mc = curl_multi_fdset (rb_http_handler->multi_handle, &fdread, &fdwrite,
-					                       &fdexcep, &maxfd);
-
-					if (mc != CURLM_OK) {
-						fprintf (stderr, "curl_multi_fdset() failed, code %d.\n", mc);
-						break;
-					}
-
-					/* On success the value of maxfd is guaranteed to be >= -1. We call
-					   select(maxfd + 1, ...); specially in case of (maxfd == -1) there are
-					   no fds ready yet so we call select(0, ...) --or Sleep() on Windows--
-					   to sleep 100ms, which is the minimum suggested value in the
-					   curl_multi_fdset() doc. */
-
-					if (maxfd == -1) {
-						/* Portable sleep for platforms other than Windows. */
-						struct timeval wait = { 0, 100 * 1000 }; /* 100ms */
-						rc = select (0, NULL, NULL, NULL, &wait);
-					} else {
-						/* Note that on some platforms 'timeout' may be modified by select().
-						   If you need access to the original value save a copy beforehand. */
-						rc = select (maxfd + 1, &fdread, &fdwrite, &fdexcep, &timeout);
-					}
-
-					switch (rc) {
-					case -1:
-						/* select error */
-						break;
-					case 0: /* timeout */
-					default: /* action */
-						curl_multi_perform (rb_http_handler->multi_handle,
-						                    &rb_http_handler->still_running);
-						break;
-					}
-				} while (rb_http_handler->still_running);
-
-				/* See how the transfers went */
-				while ((rb_http_handler->msg = curl_multi_info_read (
-				                                   rb_http_handler->multi_handle,
-				                                   &rb_http_handler->msgs_left))) {
-					if (rb_http_handler->msg->msg == CURLMSG_DONE) {
-						if (message->free_message) {
-							free (message->payload);
-						}
-						// printf ("HTTP transfer completed with status %d\n", msg->data.result);
-					}
-				}
-				free (message);
-				curl_easy_cleanup (handle);
-				curl_slist_free_all (headers);
-				rd_fifoq_elm_release (&rb_http_handler->rfq, rfqe);
+				pthread_mutex_unlock (&rb_http_handler->multi_handle_mutex);
+				// curl_slist_free_all (headers);
 			}
 		}
 	}
@@ -274,6 +204,106 @@ void * curl_send_message (void * arg) {
 	return NULL;
 }
 
+/**
+ * [curl_recv_message  description]
+ * @param  arg [description]
+ * @return     [description]
+ */
+void * curl_recv_message (void * arg) {
+
+	rd_thread_sigmask (SIG_BLOCK, SIGINT, RD_SIG_END);
+	struct rb_http_handler_s * rb_http_handler = (struct rb_http_handler_s *) arg;
+	CURLMsg * msg = NULL;
+
+	while (rb_http_handler->thread_running) {
+		while (rb_http_handler->still_running) {
+			struct timeval timeout;
+			int rc; /* select() return code */
+			CURLMcode mc; /* curl_multi_fdset() return code */
+
+			fd_set fdread;
+			fd_set fdwrite;
+			fd_set fdexcep;
+			int maxfd = -1;
+
+			long curl_timeo = -1;
+
+			FD_ZERO (&fdread);
+			FD_ZERO (&fdwrite);
+			FD_ZERO (&fdexcep);
+
+			/* set a suitable timeout to play around with */
+			timeout.tv_sec = 1;
+			timeout.tv_usec = 0;
+
+			pthread_mutex_lock (&rb_http_handler->multi_handle_mutex);
+			curl_multi_timeout (rb_http_handler->multi_handle, &curl_timeo);
+			if (curl_timeo >= 0) {
+				timeout.tv_sec = curl_timeo / 1000;
+				if (timeout.tv_sec > 1)
+					timeout.tv_sec = 1;
+				else
+					timeout.tv_usec = (curl_timeo % 1000) * 1000;
+			}
+
+			/* get file descriptors from the transfers */
+			mc = curl_multi_fdset (rb_http_handler->multi_handle, &fdread, &fdwrite,
+			                       &fdexcep, &maxfd);
+			pthread_mutex_unlock (&rb_http_handler->multi_handle_mutex);
+
+			if (mc != CURLM_OK) {
+				fprintf (stderr, "curl_multi_fdset() failed, code %d.\n", mc);
+				break;
+			}
+
+			/* On success the value of maxfd is guaranteed to be >= -1. We call
+			   select(maxfd + 1, ...); specially in case of (maxfd == -1) there are
+			   no fds ready yet so we call select(0, ...) --or Sleep() on Windows--
+			   to sleep 100ms, which is the minimum suggested value in the
+			   curl_multi_fdset() doc. */
+
+			if (maxfd == -1) {
+				/* Portable sleep for platforms other than Windows. */
+				struct timeval wait = { 0, 100 * 1000 }; /* 100ms */
+				rc = select (0, NULL, NULL, NULL, &wait);
+			} else {
+				/* Note that on some platforms 'timeout' may be modified by select().
+				   If you need access to the original value save a copy beforehand. */
+				rc = select (maxfd + 1, &fdread, &fdwrite, &fdexcep, &timeout);
+			}
+
+			switch (rc) {
+			case -1:
+				/* select error */
+				break;
+			case 0: /* timeout */
+			default: /* action */
+				pthread_mutex_lock (&rb_http_handler->multi_handle_mutex);
+				curl_multi_perform (rb_http_handler->multi_handle,
+				                    &rb_http_handler->still_running);
+				pthread_mutex_unlock (&rb_http_handler->multi_handle_mutex);
+				break;
+			}
+		}
+		/* See how the transfers went */
+		pthread_mutex_lock (&rb_http_handler->multi_handle_mutex);
+		while ((msg = curl_multi_info_read (
+		                  rb_http_handler->multi_handle,
+		                  &rb_http_handler->msgs_left))) {
+			if (msg->msg == CURLMSG_DONE) {
+				if (msg->data.result == 0) {
+					printf ("Mensaje enviado correctamente\n");
+				}
+				// printf ("HTTP transfer completed with status %d\n",
+				// msg->data.result);
+			}
+		}
+		pthread_mutex_unlock (&rb_http_handler->multi_handle_mutex);
+	}
+
+	rd_thread_cleanup ();
+	return NULL;
+}
 /**
  * Split a string (and specify the delimiter to use)
  * @param  a_str   Single sting separated by delimiter
