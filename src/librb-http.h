f64d14021ab82caaaea987d0e29f830670c1effc
Diego Fernández Barrera <bigomby@gmail.com>
2015-10-08 15:55:35 +0200

    [STYLE] Changed code style

--- src/librb-http.h
+++ src/librb-http.h
@@ -8,30 +8,34 @@
 ////////////////////
 struct rb_http_handler_s;
 struct rb_http_message_s;
-typedef void (*cb_report) (struct rb_http_handler_s * rb_http_handler,
+typedef void (*cb_report) (struct rb_http_handler_s *rb_http_handler,
                            int status_code,
-                           const char * status_code_str,
-                           char * buff, size_t bufsiz,
-                           void * opaque);
+                           long http_code,
+                           const char *status_code_str,
+                           char *buff, size_t bufsiz,
+                           void *opaque);
 
 ////////////////////
 /// Functions
 ////////////////////
-struct rb_http_handler_s * rb_http_handler (
-    const char * urls_str,
+struct rb_http_handler_s *rb_http_handler (
+    const char *urls_str,
     long curlmopt_maxconnects,
     int max_messages,
-    char * err,
+    char *err,
     size_t errbuf);
-int rb_http_handler_destroy (struct rb_http_handler_s * rb_http_handler,
-                             char * err,
+
+int rb_http_handler_destroy (struct rb_http_handler_s *rb_http_handler,
+                             char *err,
                              size_t errsize);
-int rb_http_produce (struct rb_http_handler_s * handler,
-                     char * buff,
+
+int rb_http_produce (struct rb_http_handler_s *handler,
+                     char *buff,
                      size_t len,
                      int flags,
-                     char * err,
+                     char *err,
                      size_t errsize,
-                     void * opaque);
-int rb_http_get_reports (struct rb_http_handler_s * rb_http_handler,
+                     void *opaque);
+
+int rb_http_get_reports (struct rb_http_handler_s *rb_http_handler,
                          cb_report report_fn, int timeout_ms);
