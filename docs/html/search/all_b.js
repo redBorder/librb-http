eb498007122763b6ce9b7c2dd9423a939e4b67ed
Diego Fernández Barrera <bigomby@gmail.com>
2015-10-14 16:23:57 +0200

    [DOCS] Added missing files to docs

---
+++ docs/html/search/all_b.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['url',['url',['../structrb__http__handler__s.html#a1f0df317f6eca7893bb08a54f13870ef',1,'rb_http_handler_s']]]
+];
---
+++ docs/html/search/all_c.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['verbose',['verbose',['../structrb__http__handler__s.html#a0bca537c3590135753bdd217559bb422',1,'rb_http_handler_s']]]
+];
