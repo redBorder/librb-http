5969e0b8875c782a4edf683232268a9063f22572
Diego Fernández Barrera <bigomby@gmail.com>
2015-10-14 13:09:58 +0200

    [DOCS] Updated docs and example

--- docs/html/search/all_6.js
+++ docs/html/search/all_6.js
@@ -1,4 +1,6 @@
 var searchData=
 [
+  ['p_5fthread_5frecv',['p_thread_recv',['../structrb__http__handler__s.html#a4d36d51983f56eac71371a39ee421242',1,'rb_http_handler_s']]],
+  ['p_5fthread_5fsend',['p_thread_send',['../structrb__http__handler__s.html#ae969c34a0421fe32951cb31802735f13',1,'rb_http_handler_s']]],
   ['payload',['payload',['../structrb__http__message__s.html#a70cd28f794d09eb47cf19a37fb7ba42b',1,'rb_http_message_s']]]
 ];
--- docs/html/search/variables_a.js
+++ docs/html/search/variables_a.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['urls',['urls',['../structrb__http__handler__s.html#a651ea67df6280142010181fb0f418a19',1,'rb_http_handler_s']]]
+  ['url',['url',['../structrb__http__handler__s.html#a1f0df317f6eca7893bb08a54f13870ef',1,'rb_http_handler_s']]]
 ];
