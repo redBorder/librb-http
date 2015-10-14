9c5f8bc09793206b4b9b1c77e97b17a6247bc520
Diego Fernández Barrera <bigomby@gmail.com>
2015-10-14 13:09:58 +0200

    [DOCS] Updated docs and example

--- docs/html/search/all_7.js
+++ docs/html/search/all_7.js
@@ -1,7 +1,7 @@
 var searchData=
 [
+  ['rb_5fhttp_5fcreate_5fhandler',['rb_http_create_handler',['../librb-http_8h.html#a2a79eb842642f83d2276cd42bea95eb3',1,'rb_http_create_handler(const char *urls_str, long curlmopt_maxconnects, int max_messages, char *err, size_t errbuf):&#160;rb_http_handler.c'],['../rb__http__handler_8c.html#af67cfb301ebb1e02bfa17bfecb735bab',1,'rb_http_create_handler(const char *urls_str, long curlmopt_maxconnects, int max_messages, char *err, size_t errsize):&#160;rb_http_handler.c']]],
   ['rb_5fhttp_5fget_5freports',['rb_http_get_reports',['../librb-http_8h.html#a890e981b619b1bdd5cc37fa16bb06f06',1,'rb_http_get_reports(struct rb_http_handler_s *rb_http_handler, cb_report report_fn, int timeout_ms):&#160;rb_http_handler.c'],['../rb__http__handler_8c.html#a890e981b619b1bdd5cc37fa16bb06f06',1,'rb_http_get_reports(struct rb_http_handler_s *rb_http_handler, cb_report report_fn, int timeout_ms):&#160;rb_http_handler.c']]],
-  ['rb_5fhttp_5fhandler',['rb_http_handler',['../librb-http_8h.html#a190d674d1d8cac12e0dd2445c5c8cba4',1,'rb_http_handler(const char *urls_str, long curlmopt_maxconnects, int max_messages, char *err, size_t errbuf):&#160;rb_http_handler.c'],['../rb__http__handler_8c.html#a68c2a5bd671ddb123e3f38057b5c45f6',1,'rb_http_handler(const char *urls_str, long curlmopt_maxconnects, int max_messages, char *err, size_t errsize):&#160;rb_http_handler.c']]],
   ['rb_5fhttp_5fhandler_2ec',['rb_http_handler.c',['../rb__http__handler_8c.html',1,'']]],
   ['rb_5fhttp_5fhandler_5fdestroy',['rb_http_handler_destroy',['../librb-http_8h.html#aea4aa423ff2034b027817632c746fd39',1,'rb_http_handler_destroy(struct rb_http_handler_s *rb_http_handler, char *err, size_t errsize):&#160;rb_http_handler.c'],['../rb__http__handler_8c.html#aea4aa423ff2034b027817632c746fd39',1,'rb_http_handler_destroy(struct rb_http_handler_s *rb_http_handler, char *err, size_t errsize):&#160;rb_http_handler.c']]],
   ['rb_5fhttp_5fhandler_5fexample_2ec',['rb_http_handler_example.c',['../rb__http__handler__example_8c.html',1,'']]],
@@ -11,8 +11,6 @@ var searchData=
   ['rb_5fhttp_5fmessage_5fs',['rb_http_message_s',['../structrb__http__message__s.html',1,'']]],
   ['rb_5fhttp_5fproduce',['rb_http_produce',['../librb-http_8h.html#a0d0d31d9c2dc9ed04d6722e5653b4682',1,'rb_http_produce(struct rb_http_handler_s *handler, char *buff, size_t len, int flags, char *err, size_t errsize, void *opaque):&#160;rb_http_handler.c'],['../rb__http__handler_8c.html#a0d0d31d9c2dc9ed04d6722e5653b4682',1,'rb_http_produce(struct rb_http_handler_s *handler, char *buff, size_t len, int flags, char *err, size_t errsize, void *opaque):&#160;rb_http_handler.c']]],
   ['rb_5fhttp_5freport_5fs',['rb_http_report_s',['../structrb__http__report__s.html',1,'']]],
-  ['rd_5fthread_5frecv',['rd_thread_recv',['../structrb__http__handler__s.html#ac22f049e3d329afb6b7b3eea9310fae8',1,'rb_http_handler_s']]],
-  ['rd_5fthread_5fsend',['rd_thread_send',['../structrb__http__handler__s.html#ab7856bbe03f6e21d3c4663b19d043454',1,'rb_http_handler_s']]],
   ['readme_2emd',['README.md',['../README_8md.html',1,'']]],
   ['rfq',['rfq',['../structrb__http__handler__s.html#a39160211aefb64ec052138229da98632',1,'rb_http_handler_s']]],
   ['rfq_5freports',['rfq_reports',['../structrb__http__handler__s.html#a383f2047cfcdd9fe49d0deacf770a73c',1,'rb_http_handler_s']]]
