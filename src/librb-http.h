5a72e55dce3287e8491f7d1f6d717b297f15e54f
Diego Fernández Barrera <bigomby@gmail.com>
2015-11-19 17:53:11 +0100

    [STYLE] Refactorized code

--- src/librb-http.h
+++ src/librb-http.h
@@ -1,4 +1,7 @@
 #include <string.h>
+#include <curl/curl.h>
+#include <librd/rdqueue.h>
+#include <librd/rdthread.h>
 
 #define RB_HTTP_MESSAGE_F_FREE 1
 #define RB_HTTP_MESSAGE_F_COPY 2
@@ -18,6 +21,49 @@ typedef void (*cb_report) (struct rb_http_handler_s *rb_http_handler,
                            const char *status_code_str,
                            char *buff, size_t bufsiz,
                            void *opaque);
+
+////////////////////
+// Structures
+////////////////////
+
+/**
+ *  @struct rb_http_handler_s rb_http_handler.c "rb_http_handler.c"
+ *  @brief Contains the "handler" information.
+ */
+struct rb_http_handler_s {
+	int still_running;
+	int thread_running;
+	int msgs_left;
+	int left;
+	int max_messages;
+	long timeout;
+	long connttimeout;
+	long verbose;
+	char *url;
+	CURLM *multi_handle;
+	rd_fifoq_t rfq;
+	rd_fifoq_t rfq_reports;
+	pthread_t p_thread_send;
+};
+
+/**
+ *  @struct rb_http_message_s rb_http_handler.c "rb_http_handler.c"
+ *  @brief The message to send.
+ */
+struct rb_http_message_s {
+	char *payload;
+	size_t len;
+	int free_message;
+	int copy;
+	struct curl_slist *headers;
+	void *client_opaque;
+};
+
+struct rb_http_report_s {
+	int err_code;
+	long http_code;
+	CURL *handler;
+};
 
 ////////////////////
 /// Functions
@@ -48,3 +94,5 @@ int rb_http_handler_set_opt (struct rb_http_handler_s *rb_http_handler,
                              const char *val,
                              char *err,
                              size_t errsize);
+
+void *rb_http_process_message_plain (void *arg);
