3e108e712dad137ea59d7ba9769ccf5745b6fec6
Diego Fernández Barrera <bigomby@gmail.com>
2015-12-11 12:11:57 +0100

    [IMPROVEMENT] Using another thread to get reports

--- src/rb_http_handler_example.c
+++ src/rb_http_handler_example.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-#include <signal.h>
-#include <signal.h>
-#include <librd/rdthread.h>
 #include <librbhttp/librb-http.h>
+#include "../config.h"
 
-#define MESSAGE "{\"client_mac\": \"54:26:96:db:88:01\", \"application_name\": \"wwww\", \"sensor_uuid\":\"abc\", \"a\":5}{\"client_mac\": \"54:26:96:db:88:01\", \"application_name\": \"wwww\", \"sensor_uuid\":\"abc\",\"f\":7}"
-#define N_MESSAGE 1024 * 100
+#define MESSAGE "{\"client_mac\": \"54:26:96:db:88:01\", \"application_name\": \"wwww\", \"sensor_uuid\":\"abc\", \"a\":5}"
+#define N_MESSAGE 1000 * 1
 #define URL "http://eugeniodev:2057/rbdata/def/rb_flow/"
 
-static int running = 1;
+volatile int running = 1;
+
 struct rb_http_handler_s *handler = NULL;
 
 static void my_callback (struct rb_http_handler_s *rb_http_handler,
@@ -21,23 +20,36 @@ static void my_callback (struct rb_http_handler_s *rb_http_handler,
                          size_t bufsiz,
                          void *opaque) {
 
+	(void) rb_http_handler;
+	(void) bufsiz;
+	(void) status_code_str;
+
 	if (status_code != 0) {
 		printf ("CURL CODE: %d\n", status_code);
 	}
 
 	if (status_code == 0) {
-		printf ("HTTP STATUS: %ld\n", http_status);
+		if (http_status != 200)
+			printf ("HTTP STATUS: %ld\n", http_status);
 	}
 
-	if (buff != NULL)
-		printf ("MESSAGE: %s\n\n", buff);
+	// if (buff != NULL) {
+	// 	printf ("MESSAGE: %s\n\n", buff);
+	// }
 
-	if (opaque != NULL)
-		printf ("OPAQUE: %p\n", opaque);
+	// if (opaque != NULL) {
+	// 	printf ("OPAQUE: %p\n", opaque);
+	// }
+}
 
-	(void) rb_http_handler;
-	(void) bufsiz;
-	(void) status_code_str;
+void *get_reports (void *ptr) {
+	(void) ptr;
+
+	while (running) {
+		rb_http_get_reports (handler, my_callback, 500);
+	}
+
+	return NULL;
 }
 
 int main() {
@@ -56,8 +68,11 @@ int main() {
 	int i = 0;
 
 	char *message = NULL;
+	pthread_t p_thread;
 
-	for (i = 0 ; i < N_MESSAGE && running; i++) {
+	pthread_create (&p_thread, NULL, &get_reports, NULL);
+
+	for (i = 0 ; i < N_MESSAGE; i++) {
 		while (rb_http_produce (handler,
 		                        message = strdup (MESSAGE),
 		                        strlen (MESSAGE),
@@ -65,12 +80,15 @@ int main() {
 		                        NULL,
 		                        0,
 		                        NULL) > 0 && running) {
-			rb_http_get_reports (handler, my_callback, 100);
 			free(message);
 		}
 	}
 
-	while (rb_http_get_reports (handler, my_callback, 100) && running);
+	while (rb_http_get_reports (handler, my_callback, 100));
+
+	ATOMIC_OP(sub, fetch, &running, 1);
+
+	pthread_join(p_thread, NULL);
 
 	rb_http_handler_destroy (handler, NULL, 0);
 
